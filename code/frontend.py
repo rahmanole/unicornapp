c5f269c7ef995aee76fd41a552ca634c9bd2af3e
Md Minhazur Rahman <minhaz@MacBookPro.ht.home>
2025-09-26 00:11:43 -0500

    added refresh every 10s

--- code/frontend.py
+++ code/frontend.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import helper
+import time
 
 st.set_page_config(
     page_title="Stock Screener",
@@ -27,16 +28,16 @@ rank_type = helper.get_rank_type()
 # @st.fragment
 # def load_filters():
 rank_types = {"Pre Market":"preMarket","Day":"1d","After Market":"afterMarket"}
-price = st.number_input("Minimum Price",1.0,1000.0,value = 5.0,step=0.05)
 vol = st.number_input("Minimum volume (Millions)",1.0,10.0,value = 1.0,step=0.5)
-mkt_cap = st.number_input("Minimum Maket Cap (Millions)",50.0,1000.0,value = 50.0,step=10.0)
+mkt_cap = st.number_input("Minimum Maket Cap (Millions)",50.0,100000000000.0,value = 50.0,step=10.0)
+price = st.number_input("Minimum Price",1.0,1000.0,value = 5.0,step=0.05)
 
 # vol = st.slider("Select minimum volume",1.0,10.0,0.5)
 # mkt_cap = st.slider("Select minimum market cap",50,500,10)
 st.session_state.price = round(price,2)
 st.session_state.vol = vol
 st.session_state.mkt_cap = mkt_cap
-rank = st.selectbox("Select Rank Type",rank_type,key="rank_type")
+rank = st.selectbox("Market Session",rank_type,key="rank_type")
 st.session_state.rank = rank_types[rank]
 
 
@@ -58,12 +59,14 @@ response = response.sort_values(by=["Change (%)"],ascending=False).reset_index(d
 #         acronym = helper.create_acronym(name)
 #         st.write(f"{acronym}: {name}")
 
-if "price" in st.session_state and "vol" in st.session_state and "mkt_cap" in st.session_state:
-    with st.spinner('Wait for it...'):
-        if isinstance(response,pd.DataFrame):
-            st.dataframe(response,hide_index=True)
-        else:
-            st.write("Could not load the data")
+@st.fragment(run_every="10s")
+def render_fragment():
+    if "price" in st.session_state and "vol" in st.session_state and "mkt_cap" in st.session_state:
+        with st.spinner('Wait for it...'):
+            if isinstance(response,pd.DataFrame):
+                st.dataframe(response,hide_index=True)
+            else:
+                st.write("Could not load the data")
+render_fragment()
 
-    
     
